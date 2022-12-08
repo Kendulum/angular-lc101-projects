fc53a20afdd19e9a3ef530f80740f64c71e4d086
Kendulum <ken7ma@gmail.com>
2022-12-07 20:01:28 -0600

    Chapter 29 exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaYoN9Zpk3t-sjOQxKKPBP4mPU7pLxjngnlqcRGOzD&s';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8nlwt50epDA3eD_b5Gw1G-JawRzchO8WfnDnd6uBh_lMRomNBJuSXQGcwfZs4G2dgHK4&usqp=CAU';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKDnjGzSgbdrsCMeqwc_qRjDjUrjP-ilbOTWXMPTAbHp7awhMg-Vnerk9dAvTbLJIjSoE&usqp=CAU';
 
   constructor() { }
 
