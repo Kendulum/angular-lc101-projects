93615ace0f71cfffd34d7afdf2f4cff77e160028
Kendulum <ken7ma@gmail.com>
2022-12-07 20:26:01 -0600

    Chapter 29 exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Random Photos';
   image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaYoN9Zpk3t-sjOQxKKPBP4mPU7pLxjngnlqcRGOzD&s';
   image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8nlwt50epDA3eD_b5Gw1G-JawRzchO8WfnDnd6uBh_lMRomNBJuSXQGcwfZs4G2dgHK4&usqp=CAU';
   image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKDnjGzSgbdrsCMeqwc_qRjDjUrjP-ilbOTWXMPTAbHp7awhMg-Vnerk9dAvTbLJIjSoE&usqp=CAU';
